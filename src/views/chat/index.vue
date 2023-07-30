5526c2b03aef1b8717c14b775a410a9952918c65
KaiVvv <156359332260@163.com>
2023-07-30 19:18:44 +0800

    run five

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -485,12 +485,11 @@ onUnmounted(() => {
 <!--            </div>-->
 						<div class="flex items-center flex-col justify-center mt-4 text-center ">
 							<SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-<!--							<span>Aha~</span>-->
 							<div>免费仅提供学习和测试，支持上下文，支持开启关闭联网模式，支持保存会话，切勿发布至国内平台</div>
 							<div></div>
 							<div>建站不易,使用正版3.5 API key,支持的话可以给我买一瓶冰阔落</div>
 							<div>
-<!--							<img src="src/views/images/5869b14f5e8051f1e6f86c06eed8d7d.jpg" width="200" height="100" >-->
+							<img src="http://m.qpic.cn/psc?/V10qLeg92G1M46/ruAMsa53pVQWN7FLK88i5pmY2mDPAA3.gaYcz8Vk1H0Kcdsfwvv*9pE9.iX9t9AcpFr*NxncUTHL7fSqiih6jAQti5wpWHnhzu2hVMmojE4!/b&bo=OASQBgAAAAABB4o!&rf=viewer_4" width="200" height="100" >
 							</div>
 						</div>
           </template>
