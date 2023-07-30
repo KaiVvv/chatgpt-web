b26c813d872a44a3721e8d167fda4c44babd43aa
KaiVvv <156359332260@163.com>
2023-07-30 16:02:19 +0800

    run one

--- src/views/chat/index.vue
+++ src/views/chat/index.vue
@@ -485,10 +485,10 @@ onUnmounted(() => {
 <!--            </div>-->
 						<div class="flex items-center flex-col justify-center mt-4 text-center ">
 							<SvgIcon icon="ri:bubble-chart-fill" class="mr-2 text-3xl" />
-							<span>Aha~</span>
+<!--							<span>Aha~</span>-->
 							<div>免费仅提供学习和测试，支持上下文，支持开启关闭联网模式，支持保存会话，切勿发布至国内平台</div>
 							<div></div>
-							<div>建站不易，支持的话可以给我买一瓶冰阔落</div>
+							<div>建站不易,使用正版3.5 API key,支持的话可以给我买一瓶冰阔落</div>
 							<div>
 								<img src="https://raw.githubusercontent.com/KaiVvv/chatgpt-web/main/src/views/images/5869b14f5e8051f1e6f86c06eed8d7d.jpg" width="200" height="100" alt="kele">
 							</div>
